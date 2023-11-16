d14cddd385dc55c3d0054bd80e9ed32cf4f9315e
injo <injo@injoui-noteubug.local>
2023-11-16 19:10:03 +0900

    link

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  scrollRestoration: true,
+  experimental: {
+    scrollRestoration: true,
+  },
   images: {
     remotePatterns: [
       {
