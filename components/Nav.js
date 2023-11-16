b9ee341677dbeac23107391c6e0faae8f745e958
injo <injo@injoui-noteubug.local>
2023-11-16 14:20:52 +0900

    classname

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
 import navStyles from '../styles/Nav.module.css'
-
+import { useRouter } from "next/router.js";
 const Nav = () => {
+    const router = useRouter();
+    console.log(router);
     return (
         <nav className={navStyles.nav}>
             <div id="header" className="is-fix">
@@ -16,13 +18,13 @@ const Nav = () => {
                         <div className="mweb-main-gnb">
                             <ul>
                                 <li>
-                                <a href="https://m.kbanknow.com/ib20/mnu/MWBMAN020000" className="tap-link">앱 상품</a>
+                                    <a href="https://m.kbanknow.com/ib20/mnu/MWBMAN020000" className="tap-link">앱 상품</a>
                                 </li>
                                 <li>
-                                    <Link href="/products">상품</Link>
+                                    <Link href="/products" className={router.pathname == '/products' ? 'on' : ''}>상품</Link>
                                 </li>
                                 <li>
-                                    <Link href="/gifts">혜택</Link>
+                                    <Link href="/gifts" className={router.pathname == '/gifts' ? 'on' : ''}>혜택</Link>
                                 </li>
                             </ul>
                         </div>
