0ae440dce48550a275901597370e8ae76e7dfe3f
injokly <injokly@gmail.com>
2023-11-19 14:31:52 +0900

    Gift Detail component

---
+++ components/GiftDetail.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const GiftDetail = (props) => {
+  const [data, setData] = useState("");
+  console.log(`loginingg` + JSON.stringify(props));
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const uri = `/ib20/act/CMMNOT0201000200A?ib20_media=MDA00003&joinMode=N&evntId=` + props.id;
+        let params = { evntId: props.id, joinMode: "N" };
+        console.log(`loginingg222` + JSON.stringify(props.id));
+        const response = await axios.post(`${uri}`, params);
+        setData(decodeURIComponent(response.data._msg_._body_.cntnt.replace(/\+/g, " ")));
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // 데이터를 가공하여 표시
+  const formattedData = manipulateData(data);
+
+  return <div>{formattedData}</div>;
+};
+
+// 데이터를 가공하는 함수
+const manipulateData = (data) => {
+  // 예시: 데이터에서 특정 부분을 강조하는 가공 로직
+  // 스타일을 추가할 속성
+  let additionalStyle = "display: none;";
+  // 스타일이 추가된 새로운 HTML 문자열 생성
+  console.log(`manipulateData ` + JSON.stringify(data));
+  let manipulatedData = data.replace('<div class="pc_show"', `<div class="pc_show" style="${additionalStyle}" `).replace('<div class="mob_show"', `<div class="mob_show" style="${additionalStyle}" `);
+
+  return <div dangerouslySetInnerHTML={{ __html: manipulatedData }} />;
+};
+
+export default GiftDetail;
--- pages/gift/[id].js
+++ pages/gift/[id].js
@@ -3,14 +3,15 @@ import HeadInfo from "@/components/HeadInfo";
 import { useRouter } from "next/router";
 import axios from "axios";
 import { useQuery } from "react-query";
-import { unmountComponentAtNode } from 'react-dom';
+import { unmountComponentAtNode } from "react-dom";
 import { cntntState } from "@/recoil/cntnt";
 import { useRecoilState } from "recoil";
+import GiftDetail from "@/components/GiftDetail";
 
-//dynamic routing id 값을 유지하기 위함 
+//dynamic routing id 값을 유지하기 위함
 export async function getServerSideProps(context) {
   return {
-      props: {},
+    props: {},
   };
 }
 
@@ -20,7 +21,7 @@ const Detail = () => {
   const { id } = router.query;
 
   const uri = `/ib20/act/CMMNOT0201000200A?ib20_media=MDA00003&joinMode=N&evntId=` + id;
-  let params = { evntId: id, joinMode: 'N' };
+  let params = { evntId: id, joinMode: "N" };
   //   const [eventList, setEventList] = useState([]);
   //   var event = {};
   //   useEffect(() => {
@@ -40,61 +41,59 @@ const Detail = () => {
   const [cntnt, setCntntState] = useRecoilState(cntntState);
   const [loading, setLoading] = useState(true);
 
-  const fetchData = async () => {
-    // const headers = {
-    //   'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    //   'Accept': '*/*'
-    // }
-    const response = axios.post(`${uri}`, params)
-    .then((res) => {
-      console.log("res cntnt : " + JSON.stringify(res));
-      let decodedEvent = decodeURIComponent(res.data._msg_._body_.cntnt.replace(/\+/g, " "));
-      let pCntnt = decodedEvent || {};
-      console.log("cntnt : " + pCntnt);
-      setCntntState(pCntnt);
-    });
-  };
+  // const fetchData = async () => {
+  //   // const headers = {
+  //   //   'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  //   //   'Accept': '*/*'
+  //   // }
+  //   const response = axios.post(`${uri}`, params).then((res) => {
+  //     console.log("res cntnt : " + JSON.stringify(res));
+  //     let decodedEvent = decodeURIComponent(res.data._msg_._body_.cntnt.replace(/\+/g, " "));
+  //     let pCntnt = decodedEvent || {};
+  //     console.log("cntnt : " + pCntnt);
+  //     setCntntState(pCntnt);
+  //   });
+  // };
 
-  //사용법 : const { data, isLoading, error } = useQuery(queryKey, queryFn, options)
-  const { data, isLoading } = useQuery("cntnt", fetchData, {
-    onError: (error) => {
-      console.error("request failed : ", error);
-    },
-  });
+  // //사용법 : const { data, isLoading, error } = useQuery(queryKey, queryFn, options)
+  // const { data, isLoading } = useQuery("cntnt", fetchData, {
+  //   onError: (error) => {
+  //     console.error("request failed : ", error);
+  //   },
+  // });
 
-  const removeChildren = (parent, ...children) => children.forEach(child => parent.removeChild(child))
+  // const removeChildren = (parent, ...children) => children.forEach((child) => parent.removeChild(child));
 
-  const createMarkup = () => {
-    useEffect(() => {
-      const tick = setTimeout(() => {
-        console.log(`hihihih`);
-        let parent = document.querySelector('#danger');
-        const targets = parent.querySelectorAll('.mob_show, .pc_show');
-        removeChildren(parent, ...targets);
-      }, 1000);
-    }, [])
-    return { __html: cntnt }
-  }
-  
+  // const createMarkup = () => {
+  //   useEffect(() => {
+  //     const tick = setTimeout(() => {
+  //       console.log(`hihihih`);
+  //       let parent = document.querySelector("#danger");
+  //       const targets = parent.querySelectorAll(".mob_show, .pc_show");
+  //       removeChildren(parent, ...targets);
+  //     }, 1000);
+  //   }, []);
+  //   return { __html: cntnt };
+  // };
 
   // const useLoading = (action) => {
   //   const [isLoading, setIsLoading] = useState(false);
-  
+
   //   const handleData = async (...args) => {
   //     setIsLoading(true);
-  //     return await action(...args).thfinally(() => setIsLoading(false));
+  //     return await action(...args).finally(() => setIsLoading(false));
   //   };
-  
+
   //   return [isLoading, handleData];
   // };
   // const [isLoading, createTodoLists] = useLoading(fetchData);
 
-
   return (
     <div>
       <HeadInfo></HeadInfo>
-   <div id="danger" dangerouslySetInnerHTML={createMarkup()}></div>
-    </div >
+      <GiftDetail id={id}></GiftDetail>
+      {/* <div id="danger" dangerouslySetInnerHTML={createMarkup()}></div> */}
+    </div>
   );
 };
 
